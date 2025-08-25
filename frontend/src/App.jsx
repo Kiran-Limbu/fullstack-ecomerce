f35a2a414a09bd99bab17824d0b2f3ee34817650
Kiran Limbu <limbukiran6969@gmail.com>
2025-08-25 08:38:42 +0545

    install all the packeges for the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="h-screen w-full bg-green-800">hello app</h1>
+    </div>
+  );
+};
+
+export default App;
